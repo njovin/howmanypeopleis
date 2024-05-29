1e94d09baaeb80b309aa3ea9cefef5de384dbfc6
Nathan Jovin <nathanjovin@gmail.com>
2024-05-29 00:28:58 -0700

    Initial commit, kind of

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # howmanypeopleis
+
+This is a silly project to show you how many people are in a musical group.  
+
+It's poorly-coded and inefficient.  Feel free to contribute.
---
+++ site/people.js
@@ -0,0 +1,54 @@
+let PEOPLE = [
+    {
+    "name": "Bob Moses",
+    "peopleCount": 2
+    },
+    {
+    "name": "Chris Lake",
+    "peopleCount": 1
+    },
+    {
+    "name": "Disclosure",
+    "peopleCount": 2
+    },
+    {
+    "name": "Duck Sauce",
+    "peopleCount": 2
+    },
+    {
+    "name": "Fatboy Slim",
+    "peopleCount": 1
+    },
+    {
+    "name": "Fisher",
+    "peopleCount": 1
+    },
+    {
+    "name": "Four Tet",
+    "peopleCount": 1
+    },
+    {
+    "name": "Gorgon City",
+    "peopleCount": 2
+    },
+    {
+    "name": "James Hype",
+    "peopleCount": 2
+    },
+    {
+    "name": "Lane 8",
+    "peopleCount": 1
+    },
+    {
+    "name": "Louis the Child",
+    "peopleCount": 3
+    },
+    {
+    "name": "Swedish House Mafia",
+    "peopleCount": 3
+    },
+    {
+    "name": "Walker & Royce",
+    "peopleCount": 2
+    },
+];
